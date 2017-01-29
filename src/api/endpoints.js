7216d34d9e04bf7dfdda57198e731eb3590b7a73
Alan G. Dawidowicz <alan.dawidowicz@mediamonks.com>
2017-01-29 20:21:26 -0300

    Refactor the endpoints and add tabs into the News Component

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,5 +1,8 @@
 export default {
-	news: 'https://graph.facebook.com/v2.8/28963119862/posts/?fields=attachments,link&limit=15&access_token=646098862244678%7C4WrxTQays9OoJa2PmVaiN3e-tyg',
+	news: {
+		tn: 'https://graph.facebook.com/v2.8/28963119862/posts/?fields=attachments,link&limit=15&access_token=646098862244678%7C4WrxTQays9OoJa2PmVaiN3e-tyg',
+		clarin: 'https://graph.facebook.com/v2.8/156057357762712/posts/?fields=attachments,link&limit=15&access_token=646098862244678%7C4WrxTQays9OoJa2PmVaiN3e-tyg'
+	},
 	crossfit: 'https://graph.facebook.com/v2.8/394479783985567/posts/?fields=attachments%7Bmedia%7D&limit=6&access_token=646098862244678%7C4WrxTQays9OoJa2PmVaiN3e-tyg',
 	holidays: 'http://nolaborables.com.ar/API/v1/actual'
 }
--- src/components/News.vue
+++ src/components/News.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="content section-news">
-	<md-card v-for="item in news">
-	  <md-card-media>
-	    <img v-bind:src="item.attachments.data[0].media.image.src">
-	  </md-card-media>
+	<md-tabs md-fixed>
+	  <md-tab id="news-tn" md-label="TN">
+	    <md-card v-for="item in tn">
+		  <md-card-media>
+		    <img v-bind:src="item.attachments.data[0].media.image.src">
+		  </md-card-media>
 
-	  <md-card-header>
-	    <a v-bind:href="item.link" target="_blank"><div class="md-title" v-text="item.attachments.data[0].title"></div></a>
-	    <div class="md-subhead" v-text="item.attachments.data[0].description">Subtitle here</div>
-	  </md-card-header>
-	</md-card>
+		  <md-card-header>
+		    <a v-bind:href="item.link" target="_blank"><div class="md-title" v-text="item.attachments.data[0].title"></div></a>
+		    <div class="md-subhead" v-text="item.attachments.data[0].description">Subtitle here</div>
+		  </md-card-header>
+		</md-card>
+	  </md-tab>
+
+	  <md-tab id="news-clarin" md-label="Clarin">
+	    <md-card v-for="item in clarin">
+		  <md-card-media>
+		    <img v-bind:src="item.attachments.data[0].media.image.src">
+		  </md-card-media>
+
+		  <md-card-header>
+		    <a v-bind:href="item.link" target="_blank"><div class="md-title" v-text="item.attachments.data[0].title"></div></a>
+		    <div class="md-subhead" v-text="item.attachments.data[0].description">Subtitle here</div>
+		  </md-card-header>
+		</md-card>
+	  </md-tab>
+	</md-tabs>
   </div>
 </template>
 
@@ -20,14 +37,32 @@ export default {
   name: 'news',
   data () {
     return {
-      news: null
+      tn: null,
+      clarin: null
     }
   },
   mounted: function() {
-    this.axios.get(API.news).then((response) => {
-      this.news = response.data.data;
-      console.log(this.news);
-    })
+	this.callNews("tn");
+	this.callNews("clarin");
+  },
+  methods: {
+  	callNews: function(news) {
+  		var endpoint;
+
+  		if (news == 'tn') {
+  			endpoint = API.news.tn;
+  		} else {
+  			endpoint = API.news.clarin;
+  		}
+
+  		this.axios.get(endpoint).then((response) => {
+			if (news == 'tn') {
+  				this.tn = response.data.data;
+	  		} else {
+	  			this.clarin = response.data.data;
+	  		}
+	    })
+  	}
   }
 }
 </script>
